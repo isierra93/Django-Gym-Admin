98e1d0e114ca45806545c5c4396a6693be16cc13
Ivan Sierra <sierraivan93@gmail.com>
2024-09-04 05:09:42 -0300

    Feat: add path to urls and navbar

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('clientes/', include('clientes.urls')),
-    ##path('entrenadores/', include('entrenadores.urls')),
+    path('entrenadores/', include('entrenadores.urls')),
 ]
