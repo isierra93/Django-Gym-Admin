d5453f693c2572d594239f75e638f9d6a90ca7f2
Ivan Sierra <sierraivan93@gmail.com>
2024-09-11 20:23:29 -0300

    Fix: core - changed the views to classs-based one.

--- proyecto/core/views.py
+++ proyecto/core/views.py
@@ -1,6 +1,8 @@
+from django.views.generic import View
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'core/index.html')
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'core/index.html')
