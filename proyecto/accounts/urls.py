c0c6600f22f749f8e485644750739f46b0568741
Ivan Sierra <sierraivan93@gmail.com>
2024-09-25 14:03:30 -0300

    Fix: add register

--- proyecto/accounts/urls.py
+++ proyecto/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout', LogoutView.as_view(template_name='accounts/logout.html'), name='logout')
+    path('logout', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('register', views.Register.as_view() , name='register' )
 ]
 
 ##Si aca despues creo un path que sea de PROFILE me redirecciona automaticamente sin tocar mi settings.py
--- proyecto/accounts/views.py
+++ proyecto/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+from .forms import CustomUserCreateForm
+
+class Register(CreateView):
+    form_class = CustomUserCreateForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('accounts:login')
