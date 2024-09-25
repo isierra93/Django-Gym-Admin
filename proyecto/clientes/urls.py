9cc050f9c53a7e508e7527bf655fb549e84386c4
Ivan Sierra <sierraivan93@gmail.com>
2024-09-25 16:03:10 -0300

    Fix: login required

--- proyecto/clientes/urls.py
+++ proyecto/clientes/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-
+from django.contrib.auth.decorators import login_required
 from . import views
 
 app_name = 'clientes'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index' ),
-    path('cliente/list', views.ClienteList.as_view() , name='cliente_list'),
-    path('cliente/create', views.ClienteCreate.as_view(), name='cliente_create'),
-    path('cliente/delete/<int:pk>', views.ClienteDelete.as_view(), name='cliente_delete'),
-    path('cliente/update/<int:pk>', views.ClienteUpdate.as_view(), name='cliente_update')
+    path('', login_required(views.IndexView.as_view()), name='index' ),
+    path('cliente/list', login_required(views.ClienteList.as_view()) , name='cliente_list'),
+    path('cliente/create', login_required(views.ClienteCreate.as_view()), name='cliente_create'),
+    path('cliente/delete/<int:pk>', login_required(views.ClienteDelete.as_view()), name='cliente_delete'),
+    path('cliente/update/<int:pk>', login_required(views.ClienteUpdate.as_view()), name='cliente_update')
 ]
--- proyecto/entrenadores/urls.py
+++ proyecto/entrenadores/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-
+from django.contrib.auth.decorators import login_required
 from . import views
 
 app_name = 'entrenadores'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('entrenador/list', views.EntrenadorList.as_view(), name='entrenador_list'),
-    path('entrenador/create', views.EntrenadorCreate.as_view(), name='entrenador_create'),
-    path('entrenadores/delete/<int:pk>', views.EntrenadorDelete.as_view(), name='entrenador_delete'),
-    path('entrenadores/update/<int:pk>', views.EntrenadorUpdate.as_view(), name='entrenador_update'),
-    path('clase/list', views.ClaseList.as_view(), name='clase_list'),
-    path('clase/create', views.ClaseCreate.as_view(), name='clase_create'),
-    path('clase/delete/<int:pk>', views.ClaseDelete.as_view(), name='clase_delete'),
-    path('clase/update/<int:pk>', views.ClaseUpdate.as_view(), name='clase_update'),
+    path('', login_required(views.IndexView.as_view()), name='index'),
+    path('entrenador/list', login_required(views.EntrenadorList.as_view()), name='entrenador_list'),
+    path('entrenador/create', login_required(views.EntrenadorCreate.as_view()), name='entrenador_create'),
+    path('entrenadores/delete/<int:pk>', login_required(views.EntrenadorDelete.as_view()), name='entrenador_delete'),
+    path('entrenadores/update/<int:pk>', login_required(views.EntrenadorUpdate.as_view()), name='entrenador_update'),
+    path('clase/list', login_required(views.ClaseList.as_view()), name='clase_list'),
+    path('clase/create', login_required(views.ClaseCreate.as_view()), name='clase_create'),
+    path('clase/delete/<int:pk>', login_required(views.ClaseDelete.as_view()), name='clase_delete'),
+    path('clase/update/<int:pk>', login_required(views.ClaseUpdate.as_view()), name='clase_update'),
 ]
